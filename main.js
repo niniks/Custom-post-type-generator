84b8d263aebe33a3240ae3dd6396ae99144d439e
Tim Makor <tim@yourcreativebrand.nl>
2019-04-23 15:41:14 +0200

    updates

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
         //Get values
         var name = document.getElementsByName("title")[0].value
         var nameSingle = document.getElementsByName("titleSingle")[0].value
+        var textDomain = document.getElementsByName("textDomain")[0].value
         var slug = document.getElementsByName("slug")[0].value
         var generatedCodeDiv = document.getElementById('generatedCode');
         
@@ -16,25 +17,25 @@
 */<br>
 function codex_book_init() { <br>
 $labels = array( <br>
-    'name'               => _x( '`+name+`', 'post type general name', 'your-plugin-textdomain' ),<br>
-    'singular_name'      => _x( '`+nameSingle+`', 'post type singular name', 'your-plugin-textdomain' ),<br>
-    'menu_name'          => _x( '`+name+`', 'admin menu', 'your-plugin-textdomain' ),<br>
-    'name_admin_bar'     => _x( '`+nameSingle+`', 'add new on admin bar', 'your-plugin-textdomain' ),<br>
-    'add_new'            => _x( 'Add New', '`+nameSingle.toLowerCase()+`', 'your-plugin-textdomain' ),<br>
-    'add_new_item'       => __( 'Add New `+nameSingle+`', 'your-plugin-textdomain' ),<br>
-    'new_item'           => __( 'New `+nameSingle+`', 'your-plugin-textdomain' ),<br>
-    'edit_item'          => __( 'Edit `+nameSingle+`', 'your-plugin-textdomain' ),<br>
-    'view_item'          => __( 'View `+nameSingle+`', 'your-plugin-textdomain' ),<br>
-    'all_items'          => __( 'All `+name+`', 'your-plugin-textdomain' ),<br>
-    'search_items'       => __( 'Search `+name+`', 'your-plugin-textdomain' ),<br>
-    'parent_item_colon'  => __( 'Parent `+name+`:', 'your-plugin-textdomain' ),<br>
-    'not_found'          => __( 'No `+name+` found.', 'your-plugin-textdomain' ),<br>
-    'not_found_in_trash' => __( 'No `+name+` found in Trash.', 'your-plugin-textdomain' )<br>
+    'name'               => _x( '`+name+`', 'post type general name', '`+textDomain+`' ),<br>
+    'singular_name'      => _x( '`+nameSingle+`', 'post type singular name', '`+textDomain+`' ),<br>
+    'menu_name'          => _x( '`+name+`', 'admin menu', '`+textDomain+`' ),<br>
+    'name_admin_bar'     => _x( '`+nameSingle+`', 'add new on admin bar', '`+textDomain+`' ),<br>
+    'add_new'            => _x( 'Add New', '`+nameSingle.toLowerCase()+`', '`+textDomain+`' ),<br>
+    'add_new_item'       => __( 'Add New `+nameSingle+`', '`+textDomain+`' ),<br>
+    'new_item'           => __( 'New `+nameSingle+`', '`+textDomain+`' ),<br>
+    'edit_item'          => __( 'Edit `+nameSingle+`', '`+textDomain+`' ),<br>
+    'view_item'          => __( 'View `+nameSingle+`', '`+textDomain+`' ),<br>
+    'all_items'          => __( 'All `+name+`', '`+textDomain+`' ),<br>
+    'search_items'       => __( 'Search `+name+`', '`+textDomain+`' ),<br>
+    'parent_item_colon'  => __( 'Parent `+name+`:', '`+textDomain+`' ),<br>
+    'not_found'          => __( 'No `+name+` found.', '`+textDomain+`' ),<br>
+    'not_found_in_trash' => __( 'No `+name+` found in Trash.', '`+textDomain+`' )<br>
 );<br><br>
 
 $args = array(<br>
     'labels'             => $labels,<br>
-    'description'        => __( 'Description.', 'your-plugin-textdomain' ),<br>
+    'description'        => __( 'Description.', '`+textDomain+`' ),<br>
     'public'             => true,<br>
     'publicly_queryable' => true,<br>
     'show_ui'            => true,<br>
@@ -51,5 +52,27 @@ $args = array(<br>
 register_post_type( 'book', $args );<br>
 }`;
 plainCode = '<code>'+plainCode+'</code>'
-        generatedCodeDiv.innerHTML = plainCode;
+generatedCodeDiv.innerHTML = plainCode;
+
     }
+
+    function copyCode() {
+            //Select code content
+            var copyText = document.querySelector("code").textContent;
+            // Create temp textarea for copy code
+            const el = document.createElement('textarea');
+            //Select code
+            el.value = copyText;
+            //Temp add to body
+            document.body.appendChild(el);
+            //Now select the code
+            el.select();
+            //Copy it to system clipboard
+            document.execCommand('copy');
+            //Remove temp el in body
+            document.body.removeChild(el);
+            //Alert user about success copy
+            alert("Copied to clipboard!");
+    }
+
+    
